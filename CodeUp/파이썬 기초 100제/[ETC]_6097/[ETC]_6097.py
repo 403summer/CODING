b2717cb8faba79c41ef0b31b44e79792f9748c2d
summer <4003summer@gmail.com>
2022-07-04 23:18:53 +0900

    성공

--- CodeUp/파이썬 기초 100제/[ETC]_6097/[ETC]_6097.py
+++ CodeUp/파이썬 기초 100제/[ETC]_6097/[ETC]_6097.py
@@ -2,20 +2,20 @@
 
 h, w = map(int,input().split())
 
-list = [[0 for i in range(h)] for i in range(w)]
+list = [[0 for i in range(w)] for i in range(h)]
 
 n = int(input())
 for i in range(n):
     l, d, x, y = map(int,input().split())
     for j in range(l):
         if d == 0:
-            list[x+j][y] = 1
+            list[x-1][y-1+j] = 1
         else:
-            list[x][y+j] = 1
+            list[x-1+j][y-1] = 1
 
 for i in range(h):
     for j in range(w):
-        if j == h:
+        if j+1 == w:
             print(list[i][j])
         else:
             print(list[i][j], end=' ')
